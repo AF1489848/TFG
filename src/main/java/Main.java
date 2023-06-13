447a6576ee4d2fac290a5fa38a30097ddc93931e
feralejandro <alejandro.fernandezgar@autonoma.cat>
2023-06-13 14:41:21 +0200

    finalizar creación del Script

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
         String scriptPrefix = "job_script"; // Prefijo para los nombres de los scripts
-        String outputDirectory = "C:\\Users\\aleja\\OneDrive - UAB\\TFG\\TFG_ScriptsAutom\\target\\Scripts"; // Directorio de salida para los scripts
+        String outputDirectory = "./target/Scripts"; // Directorio de salida para los scripts
 
 
         String scriptName = scriptPrefix + ".sub";
